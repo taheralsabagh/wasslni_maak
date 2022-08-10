b2224a4fdcb9dfcb72b4e3c872f09de106474c47
Taher_Alsabagh <taher.alsabagh@gmail.com>
2022-08-10 20:43:30 +0300

    update users migrate 3

--- database/migrations/2022_08_10_163309_create_vehicles_table.php
+++ database/migrations/2022_08_10_163309_create_vehicles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('vehicles', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string("name");
             $table->string("image");
             $table->integer("max_passengers");
